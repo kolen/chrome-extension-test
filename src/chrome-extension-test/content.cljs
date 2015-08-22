c4adef46fea59517ab90d38afdcb21fd2515958f
kolen <incredible.angst@gmail.com>
2015-08-23 01:35:57 +0300

    Added observe-text, fixed observe

--- src/chrome-extension-test/content.cljs
+++ src/chrome-extension-test/content.cljs
@@ -2,23 +2,36 @@
   (:require [khroma.runtime :as runtime]
             [khroma.log :as console]
             [cljs.core.async :refer [>! <!]]
+            [cljs.core.async :as async]
             [weasel.repl]
+            [dommy.core :as dommy]
             [dommy.core :refer-macros [sel sel1]]
-            [cljs.core.async :refer [put! chan <!]])
+            [cljs.core.async :refer [put! chan <! pipe]])
   (:require-macros [cljs.core.async.macros :refer [go]]))
 
 (defn init []
   (let [bg (runtime/connect)]
     (weasel.repl/connect "ws://localhost:11000" :verbose true)
     (go (>! bg :lol-i-am-a-content-script)
-        (console/log "Background said: " (<! bg)))))
+        (console/log "Background said: " (<! bg))
 
-(defn ^:export observe [selector options]
-  (let [c (chan)
-        mut-callback! (fn [records zis] (doseq [r records] put! c r))
+        (observe :#ac_performer {}))))
+
+(defn observe [selector options]
+  (let [ch (chan)
+        mut-callback! (fn [records zis] (doseq [r records] (put! ch r)))
         obs (js/MutationObserver. mut-callback!)]
     (.observe obs (sel1 selector) (clj->js options))
-    c))
+    ch))
+
+(defn observe-text [selector]
+  "Observe node text contents"
+  (let [ch (observe selector {:characterData true :subtree true})
+        extract-text (fn [el] (-> el (aget "target") (dommy/text)))]
+    (async/map extract-text [ch])))
+
+; (def ch (observe-text :#ac_performer))
+; (go (while true (.log js/console (<! ch))))
 
 (defn ^:export
   repl-connect []
